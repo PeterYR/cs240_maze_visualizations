85e3de971b5750d3bbe27ca08bbe8ae59fe6d29d
Peter Z <ziyuzhang01@gmail.com>
2022-05-07 21:57:34 -0500

    request and cache maze data

--- maze/run.py
+++ maze/run.py
@@ -1,4 +1,23 @@
+import json
+import requests
 from drawmaze import MazeBlock
+from PIL import Image
+
+try:
+    data = requests.get('http://sp22-cs240-adm.cs.illinois.edu:24000/mazeState').json()
+    print(f'Loaded {len(data)} blocks')
+    with open('maze_data.json', 'w') as fp:
+        json.dump(data, fp)
+except:
+    with open('maze_data.json', 'r') as fp:
+        data = json.load(fp)
+    print(f'Connection failed, loaded {len(data)} blocks from `maze_data.json`')
+
+#TODO: find minimum row and col, adjust data to have non-negative coords
+
+
+
+
 
 # 503 error segment
 data = ["9a8088c","5b02024","5b49494","0a02020","5b4d1e5","1e571e5","3a282a6"]
